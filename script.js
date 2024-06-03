2aa1631bbe6b1a5e5303f556615c6b8245447c67
JoshuaIsADev <joshuaisadev@gmail.com>
2024-06-02 22:15:20 -0700

    finished css, layout and responsive layout

--- script.js
+++ script.js
@@ -3,7 +3,93 @@
 // Data
 const projects = [
   {
+    name: 'The MET Gallery',
+    year: '2023',
+    image: '#image',
+    paragraphs: [
+      'As an art enthusiast, I wanted my first vanilla javascript project to use the MET Museum API to display some of their art on the web. Some features I made include allowing users to resize the art, hide information, and detect if they are on mobile to show small size images for faster load times.',
+    ],
+    githubLink: '#github',
+    liveLink: '#live',
+    liveUserNameGuest: '',
+    livePasswordGuest: '',
+    liveUserNameAdmin: '',
+    livePasswordAdmin: '',
+    technical: ['Vanila Javascript', 'CSS', 'HTML', 'Responsive', 'Figma'],
+    client: 'n/a',
+    featuresUser: [
+      'View artwork from The MET Api',
+      'Enlarge/minimize artwork',
+      'Hide/show information',
+      'Show next artwork',
+      'Small image in mobile',
+    ],
+    featuresAdmin: [],
+  },
+
+  {
+    name: 'Slide',
+    year: '2023',
+    image: '#image',
+    paragraphs: [
+      'One of my friends had asked me about financial budgeting basics. Rather than just tell her about it, I built a simple to use javascript app! The user inputs their salary, and it will recommend how much they should spend on items such as housing, food, transportation, etc. The app is called Slide because I added the ability for the user to adjust the values. They can also add and delete fields to their liking.',
+    ],
+    githubLink: '#github',
+    liveLink: '#live',
+    liveUserNameGuest: '',
+    livePasswordGuest: '',
+    liveUserNameAdmin: '',
+    livePasswordAdmin: '',
+    technical: ['Vanila Javascript', 'CSS', 'HTML', 'Responsive', 'Figma'],
+    client: 'n/a',
+    featuresUser: [
+      'Input number',
+      'Adjust budget with sliders',
+      'Add category',
+      'Delete category',
+    ],
+    featuresAdmin: [],
+  },
+
+  {
+    name: 'Maikallaneza.com',
+    year: '2024',
+    image: '#image',
+    paragraphs: [
+      'This was my first React project, which I built for a published author. Since she occasionally updates her bio and literary works, I decided to build her a custom content manager in addition to her landing page. The challenge was then to allow her to sign in, create, update, and delete her bio, photo, and literary works.',
+      'She also requested that her site be accessible. I made her site responsive to mobile screens and readers and tested it for accessibility. Her website is currently live to show her fans, prospective editors and publishers.',
+    ],
+    githubLink: '#github',
+    liveLink: '#live',
+    liveUserNameGuest: '',
+    livePasswordGuest: '',
+    liveUserNameAdmin: '',
+    livePasswordAdmin: '',
+    technical: [
+      'React',
+      'Javascript',
+      'CSS',
+      'Responsive',
+      'Styled components',
+      'React query',
+      'React hook forms',
+      'React router',
+      'Supabase',
+      'Figma',
+    ],
+    client: 'Maika Llaneza',
+    featuresUser: ['Read data from Supabase'],
+    featuresAdmin: [
+      'Add bio and projects',
+      'Edit bio projects, and photo',
+      'Delete projects',
+      'Sign in/out',
+    ],
+  },
+
+  {
     name: 'Magis',
+    year: '2024',
     image: '#image',
     paragraphs: [
       'I was inspired to make an ecommerce app after seeing products from this designer furniture company. As a design enthusiast, I was excited to create a digital experience that reflects this company’s forward-thinking aesthetics.',
@@ -48,40 +134,6 @@ const projects = [
       'Sign in/out',
     ],
   },
-  {
-    name: 'Maikallaneza.com',
-    image: '#image',
-    paragraphs: [
-      'This was my first React project, which I built for a published author. Since she occasionally updates her bio and literary works, I decided to build her a custom content manager in addition to her landing page. The challenge was then to allow her to sign in, create, update, and delete her bio, photo, and literary works.',
-      'She also requested that her site be accessible. I made her site responsive to mobile screens and readers and tested it for accessibility. Her website is currently live to show her fans, prospective editors and publishers.',
-    ],
-    githubLink: '#github',
-    liveLink: '#live',
-    liveUserNameGuest: '',
-    livePasswordGuest: '',
-    liveUserNameAdmin: '',
-    livePasswordAdmin: '',
-    technical: [
-      'React',
-      'Javascript',
-      'CSS',
-      'Responsive',
-      'Styled components',
-      'React query',
-      'React hook forms',
-      'React router',
-      'Supabase',
-      'Figma',
-    ],
-    client: 'Maika Llaneza',
-    featuresUser: ['Read data from Supabase'],
-    featuresAdmin: [
-      'Add bio and projects',
-      'Edit bio projects, and photo',
-      'Delete projects',
-      'Sign in/out',
-    ],
-  },
 ];
 
 let find = document.querySelector('.projects-container');
@@ -90,91 +142,95 @@ const containerProjects = document.querySelector('.projects-container');
 
 const displayProjects = function (projects) {
   projects.forEach((proj, i) => {
-    console.log(proj);
+    // console.log(proj);
     const html = `
       <article class="project-wrapper">
         <h2 class="font-l">${proj.name}</h2>
-        <img alt="${proj.name} project screenshot" src=${proj.image} />
-        <div class="project-info-container">
+        <p class="font-s">${proj.year}</p>
+        <img alt="${proj.name} project screenshot" src=${
+      proj.image
+    } class="project-img"/>
           <div class="about-container">
-            <h3>About project</h3>
-            ${proj.paragraphs
-              .map((paragraph) => `<p>${paragraph}</p>`)
-              .join('')}
-            
+            <h3 class="font-s">About project</h3>
+            <div class="about-wrapper">
+              ${proj.paragraphs
+                .map((paragraph) => `<p>${paragraph}</p>`)
+                .join('')}
+            </div>
           </div>
-          <div class="links-container">
-            <h3>Links</h3>
-            <ul class="vertical-ul">
-              <a href=${proj.githubLink}>Github</a>
-            </ul>
-            <ul class="vertical-ul">
-              <a href=${proj.liveLink}>View live</a>
-            </ul>
-            ${
-              proj.liveUserNameAdmin.length > 0
-                ? `<ul class='vertical-ul'>
-                  <li>(consumer login)</li>
-                  <li>username: ${proj.liveUserNameAdmin}</li>
-                  <li>password: ${proj.liveUserPasswordAdmin}</li>
-                </ul>`
-                : ''
-            }
-            ${
-              proj.liveUserNameGuest.length > 0
-                ? `<ul class='vertical-ul'>
-                  <li>(consumer login)</li>
-                  <li>username: ${proj.liveUserNameGuest}</li>
-                  <li>password: ${proj.liveUserPasswordGuest}</li>
-                </ul>`
-                : ''
-            }
+          <div class="details-container">
+            <div class="links-container">
+              <h3 class="font-s">Links</h3>
+              <ul class="vertical-ul">
+                <a href=${proj.githubLink}>Github</a>
+              </ul>
+              <ul class="vertical-ul">
+                <a href=${proj.liveLink}>View live</a>
+              </ul>
+              ${
+                proj.liveUserNameAdmin.length > 0
+                  ? `<ul class='vertical-ul'>
+                    <li>(consumer login)</li>
+                    <li>username: ${proj.liveUserNameAdmin}</li>
+                    <li>password: ${proj.liveUserPasswordAdmin}</li>
+                  </ul>`
+                  : ''
+              }
+              ${
+                proj.liveUserNameGuest.length > 0
+                  ? `<ul class='vertical-ul'>
+                    <li>(consumer login)</li>
+                    <li>username: ${proj.liveUserNameGuest}</li>
+                    <li>password: ${proj.liveUserPasswordGuest}</li>
+                  </ul>`
+                  : ''
+              }
+            </div>
+            <div class="technical-container">
+              <h3 class="font-s">Technical</h3>
+                <ul class="technical-ul">
+                ${proj.technical
+                  .map((technical) => `<li>${technical}</li>`)
+                  .join('')}
+                </ul>
+            </div>
           </div>
-          <div class="technical-container">
-          <h3>Technical</h3>
-            <ul class="technical">
-            ${proj.technical
-              .map((technical) => `<li class="technical-li">${technical}</li>`)
-              .join('')}
-            </ul>
-          </div>
-          <div class="client-container">
-            <h3>Client</h3>
-            <ul class="technical">
-              <li>
-                ${proj.client}
-              </li>
-            </ul>
-          </div>
-          <div class="features-container">
-            <h3>Features</h3>
-            ${
-              proj.featuresAdmin.length > 0
-                ? `<h2>Admin features</h2>
-            <ul class="vertical-ul">
-            ${proj.featuresAdmin
-              .map((adminFeatures) => `<li>${adminFeatures}</li>`)
-              .join('')}
-            </ul>`
-                : ''
-            }
-            ${
-              proj.featuresUser.length > 0
-                ? `<h2>User features</h2>
-            <ul class="vertical-ul">
-            ${proj.featuresUser
-              .map((userFeatures) => `<li>${userFeatures}</li>`)
-              .join('')}
-            </ul>`
-                : ''
-            }
-            
-          </div>
-        </div>
+          <aside class="project-aside">
+            <div class="client-container">
+              <h3 class="font-s">Client</h3>
+              <ul class="technical">
+                <li>
+                  ${proj.client}
+                </li>
+              </ul>
+            </div>
+            <div class="features-container">
+              <h3 class="font-s">Features</h3>
+              ${
+                proj.featuresAdmin.length > 0
+                  ? `<h4 class="font-xs">Admin features</h4>
+              <ul class="vertical-ul">
+              ${proj.featuresAdmin
+                .map((adminFeatures) => `<li>${adminFeatures}</li>`)
+                .join('')}
+              </ul>`
+                  : ''
+              }
+              ${
+                proj.featuresUser.length > 0
+                  ? `<h4 class="font-xs">User features</h4>
+              <ul class="vertical-ul">
+              ${proj.featuresUser
+                .map((userFeatures) => `<li>${userFeatures}</li>`)
+                .join('')}
+              </ul>`
+                  : ''
+              }
+            </div>
+          </aside>
       </article>
     `;
-
-    containerProjects.insertAdjacentHTML('beforebegin', html);
+    containerProjects.insertAdjacentHTML('afterbegin', html);
   });
 };
 
