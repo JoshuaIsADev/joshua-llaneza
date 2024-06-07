25b397610a72bebdb0726bef65dc6bd0be6195c9
JoshuaIsADev <joshuaisadev@gmail.com>
2024-06-06 17:39:34 -0700

    fixed links

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const projects = [
     paragraphs: [
       'As an art enthusiast, I wanted my first vanilla javascript project to use the MET Museum API to display some of their art on the web. This app fetched a list of artwork then fetched more information about the art such as image and title. Users can enlarge the art to show more detail, and they can hide information. This app will detect if they are on mobile to show small size images for faster load times.',
     ],
-    githubLink: '#github',
-    liveLink: '#live',
+    githubLink: 'https://github.com/JoshuaIsADev/The-MET-v3',
+    liveLink: 'https://the-met-gallery.netlify.app/',
     liveUserNameGuest: '',
     livePasswordGuest: '',
     liveUserNameAdmin: '',
@@ -39,10 +39,10 @@ const projects = [
     year: '2023',
     image: './content/img/screenshot_slide01.jpg',
     paragraphs: [
-      'One of my friends had asked me about financial budgeting basics. Rather than just tell her about it, I built a simple to use javascript app! This is a mobile-first app where the user inputs their salary, and it will recommend how much they should spend on items such as housing, food, transportation, etc. The app is called Slide because I added the ability for the user to adjust the values. They can also add and delete fields to their liking.',
+      'One of my friends had asked me about financial budgeting basics. Rather than just tell them about it, I built a simple to use javascript app! This is a mobile-first app where the user inputs their salary, and it will recommend how much they should spend on items such as housing, food, transportation, etc. The user can also slide the sliders to adjust the values, add, and delete fields to their liking.',
     ],
-    githubLink: '#github',
-    liveLink: '#live',
+    githubLink: 'https://github.com/JoshuaIsADev/Slide',
+    liveLink: 'http://slide-budget.netlify.app',
     liveUserNameGuest: '',
     livePasswordGuest: '',
     liveUserNameAdmin: '',
@@ -64,13 +64,13 @@ const projects = [
     image: './content/img/screenshot_ml.png',
     paragraphs: [
       'This was my first React project, which I built for a published author. Since she occasionally updates her bio and literary works, I decided to build her a custom content manager in addition to her landing page. The challenge was then to allow her to sign in, create, update, and delete her bio, photo, and literary works.',
-      'She also requested that her site be accessible. I made her site responsive to mobile screens and readers and tested it for accessibility. Her website is currently live to show her fans, prospective editors and publishers.',
+      'She also requested that her site be accessible so I tested it for accessibility. It has a score of 100 from Google LightHouse. Her website is currently live to show her fans, prospective editors and publishers.',
     ],
-    githubLink: '#github',
-    liveLink: '#live',
+    githubLink: 'https://github.com/JoshuaIsADev/Maika-Llaneza-v2',
+    liveLink: 'http://maikallaneza.com',
     liveUserNameGuest: '',
     livePasswordGuest: '',
-    liveUserNameAdmin: '',
+    liveUserNameAdmin: 'CMS demo available during interview',
     livePasswordAdmin: '',
     technical: [
       'React',
@@ -83,7 +83,6 @@ const projects = [
       'React hook forms',
       'React router',
       'Supabase(RESTful API)',
-      'CRUD app',
       'High accessibility score',
       'Figma',
     ],
@@ -105,12 +104,12 @@ const projects = [
       'As a design enthusiast, I was interested to create a digital experience that reflects this company’s artistic aesthetics. I looked at their current website (which is made for industry professionals) and wanted to create a mock consumer website with ecommerce features such as selecting colors, adding, editing, deleting items in a shopping cart, and signing in/out checking previous orders.',
       'I further challenged myself by creating a content management app, where the business can sign in/out, then add, edit, and delete products, images, and variants of those products.',
     ],
-    githubLink: '#github',
-    liveLink: '#live',
-    liveUserNameGuest: ' guest@test.com',
-    livePasswordGuest: 'guest123',
-    liveUserNameAdmin: ' guest@test.com',
-    livePasswordAdmin: 'guest123',
+    githubLink: 'https://github.com/JoshuaIsADev/magis',
+    liveLink: 'https://magis-portfolio-project.netlify.app/',
+    liveUserNameGuest: 'guestuser@email.com',
+    livePasswordGuest: 'GuestUser!1234',
+    liveUserNameAdmin: 'CMS demo available during interview',
+    livePasswordAdmin: '',
     technical: [
       'React',
       'Javascript',
@@ -123,7 +122,6 @@ const projects = [
       'React router',
       'Context Api',
       'Supabase(RESTful API)',
-      'CRUD app',
       'Figma',
     ],
     client:
@@ -173,26 +171,30 @@ const displayProjects = function (projects) {
             <div class="links-container">
               <h3 class="font-s">Links</h3>
               <ul class="vertical-ul">
-                <a href=${proj.githubLink}>Github</a>
+                <a href=${proj.githubLink} target="_blank">Github</a>
               </ul>
               <ul class="vertical-ul">
-                <a href=${proj.liveLink}>View live</a>
+                <a href=${proj.liveLink} target="_blank">View live</a>
               </ul>
               ${
                 proj.liveUserNameAdmin.length > 0
                   ? `<ul class='vertical-ul'>
-                    <li>(consumer login)</li>
-                    <li>username: ${proj.liveUserNameAdmin}</li>
-                    <li>password: ${proj.liveUserPasswordAdmin}</li>
+                    <li><h4 class="font-xs">Admin login</h4></li>
+                    <li>Username: ${proj.liveUserNameAdmin}</li>
+                     ${
+                       proj.livePasswordAdmin.length > 0
+                         ? `<li>Password: ${proj.liveUserPasswordAdmin}`
+                         : ''
+                     }</li>
                   </ul>`
                   : ''
               }
               ${
                 proj.liveUserNameGuest.length > 0
                   ? `<ul class='vertical-ul'>
-                    <li>(consumer login)</li>
-                    <li>username: ${proj.liveUserNameGuest}</li>
-                    <li>password: ${proj.liveUserPasswordGuest}</li>
+                    <li><h4 class="font-xs">Consumer login</h4></li>
+                    <li>Username: ${proj.liveUserNameGuest}</li>
+                    <li>Password: ${proj.livePasswordGuest}</li>
                   </ul>`
                   : ''
               }
