a971291f680e0a78f53e8494979aa7a193e93a6c
JoshuaIsADev <joshuaisadev@gmail.com>
2024-09-14 23:22:08 -0700

    updated website

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 'use strict';
 
 // Data
+const title = { name: 'Joshua Llaneza', url: '' };
+
+const navigation = [
+  { name: 'About', url: './index.html' },
+  { name: 'Landscape Architecture', url: './landscapearchitecture.html' },
+  { name: 'Photography', url: './photography.html' },
+  { name: 'Web Development', url: './webdevelopment.html' },
+  { name: 'Email', url: 'mailto:joshuallaneza@outlook.com' },
+];
+
+const footer = 'Website and projects - copyright © Joshua Llaneza 2024';
+
 const projects = [
   {
     name: 'The MET Gallery',
@@ -148,6 +160,45 @@ const projects = [
 ];
 
 let find = document.querySelector('.projects-container');
+
+const containerHeader = document.querySelector('.header-container');
+const containerHeaderNav = document.querySelector('.header-nav');
+const containerFooter = document.querySelector('.footer-container');
+
+const displayHeader = function (title) {
+  const html = `
+    <div class="text-row">
+        <h1 class="font-l">${title.name}</h1>
+      </div>
+    `;
+  containerHeader.insertAdjacentHTML('afterbegin', html);
+};
+
+const displayHeaderNav = function (navigation) {
+  navigation.forEach((nav, i) => {
+    const html = `
+    <li class="text-row">
+    <a href="${nav.url}" class="font-l">${nav.name}</a>
+    </li>
+    `;
+    containerHeaderNav.insertAdjacentHTML('beforeBegin', html);
+  });
+};
+
+const displayFooter = function (footer) {
+  const html = `
+   <div class="text-row">
+      <p class="font-l">
+        ${footer}
+      </p>
+    </div>
+    `;
+  containerFooter.insertAdjacentHTML('afterbegin', html);
+};
+
+displayHeader(title);
+displayHeaderNav(navigation);
+displayFooter(footer);
 
 const containerProjects = document.querySelector('.projects-container');
 
